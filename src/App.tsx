9fcab4efb8db2eafd98cd9aa6d3390b4c9cee5b4
govind-t <govindasamy.krishnan@tarento.com>
2022-01-19 04:10:15 +0530

    google authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import { GoogleSignInComponent } from './GoogleSignInComponent';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const [selectedMenuItem, setSelectedMenuItem] = useState<MenuAction>();
+  const [googleAccessToken, setGoogleAccessToken] = useState<string>('');
+
+  // const menuSelectionChanged: MenuActionListener = function (action: MenuAction): void {
+  //   setSelectedMenuItem(action);
+  //   if (action === MenuAction.LOGOUT) {
+  //     setGoogleAccessToken('');
+  //   }
+  // };
+
+  let componentToDisplay;
+
+  if (!googleAccessToken) {
+    componentToDisplay = <GoogleSignInComponent loginSuccess={(response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+      if ('tokenId' in response) {
+        setGoogleAccessToken(response.tokenId);
+      }
+    }} />
+  }else {
+    componentToDisplay = <h1><span className="badge badge-pill badge-danger align-items-centre">Please select an item to display</span></h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {googleAccessToken &&
+        "Welcome"
+      }      
+      {componentToDisplay}
+    </>
   );
 }
 
