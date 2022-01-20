dd88f9042c46c925dcc538330ed72de32a36318a
Mrigank Shekhar Shringi <mrigank.shringi@tarento.com>
2022-01-20 15:51:37 +0530

    added check session function to app.tex

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import Login from './pages/Login';
 import SideMenu from './Components/SideMenu';
+
 class App extends React.Component{
 
+  
+  state = {
+    isLoggedIn : false,
+  }
+
   componentDidMount(){
+    this.checkSession()
+  }
+
+  checkSession(){
+    let authToken = localStorage.getItem('authToken');
+    if(authToken != null || authToken != undefined){
+      this.setState({isLoggedIn : true});
+    } else {
+      this.setState({isLoggedIn : false});
     }
+  }
+
   render(){
-    return(
-      <>
-        <Login />
-        <SideMenu/>
-      </>
-    )
+    if(!this.state.isLoggedIn){
+      return <Login onClick={()=>this.checkSession()}/>
+    } else {
+      return <SideMenu />
+    }
   }
 }
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import {auth , provider}  from '../firebase';
 import classes from './Login.module.css';
 import GoogleLoginIcon from '../assets/GoogleLoginIcon.png'
+import { type } from "os";
 
-const signInwithGoogle = ()=>{
+type childProps = {
+    onClick?: () => void
+}
+
+const Login : React.FC<childProps> = ({
+    onClick=()=>{}
+}) => {
+    const signInwithGoogle = ()=>{
     auth.signInWithPopup(provider)
     .then((res:any)=>{
       console.log("login response", res);
+      console.log("token", res.credential.accessToken);
+      localStorage.setItem('authToken',  res.credential.accessToken);
+      localStorage.setItem('userProfilePic', res.additionalUserInfo.profile.picture);
+      onClick();
     }).catch(alert);
   }
-
-const Login : React.FC = () => {
     return(
         <div className={classes.container}>
             <button
