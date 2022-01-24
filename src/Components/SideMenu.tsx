c9ccdf3d30712dddbe546290576c216d38de0b9e
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-01-24 11:16:00 +0530

    added contributors file

--- src/Components/SideMenu.tsx
+++ src/Components/SideMenu.tsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Drawer from '@material-ui/core/Drawer'
 import AppMenu from './AppMenu';
+import Box from '@mui/material/Box';
 
 import Buttonss from './Button'
 const App: React.FC = () => {
@@ -12,18 +13,18 @@ const App: React.FC = () => {
   return (
     <div className={clsx('App', classes.root)}>
       <CssBaseline />
-     
-      <Drawer 
+     <Box
+      
       //style={{float:'right'}}
-        variant="permanent"
+       // variant="permanent"
         classes={{
           paper: classes.drawerPaper,
         }}
       >
-     <AppMenu  /> 
+      <AppMenu  /> 
     
-      </Drawer>
-     
+    
+      </Box>
     </div>
   )
 }
@@ -33,18 +34,16 @@ const App: React.FC = () => {
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-   
+  
   
     
   },
   drawerPaper: {
     position: 'relative',
-   
-   
+  
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-    background: 'white',
-    color: 'black',
+    background: "transparent",
    
   },
  
